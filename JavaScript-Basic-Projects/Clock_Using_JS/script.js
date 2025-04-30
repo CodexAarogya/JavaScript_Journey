1731a693d1340d8d517c4343cb7d5b8b542e3ef9
Aarogya-Parajuli <aarogyaparajuli07@gmail.com>
2025-04-30 14:37:51 +0545

    analogClock_updated UI elements

---
+++ JavaScript-Basic-Projects/Clock_Using_JS/script.js
@@ -0,0 +1,32 @@
+let hr = document.getElementById('hour');
+let min = document.getElementById('minute');
+let sec = document.getElementById('second');
+
+function getTime(){
+    const myDate = new Date();
+    let hh = myDate.getHours() % 12;
+    let mm = myDate.getMinutes();
+    let ss = myDate.getSeconds();
+
+    let hrMove = 30*hh;
+    let minMove = 6*mm;
+    let secMove = 6*ss
+
+    console.log(hh,mm,ss);
+    console.log(hrMove,minMove,secMove);  
+
+    hr.style.transform = `rotate(${hrMove}deg)`;
+    min.style.transform = `rotate(${minMove}deg)`;
+    sec.style.transform = `rotate(${secMove}deg)`;
+}
+
+setInterval(getTime,1000);
+
+let day = document.getElementById('daybox');
+let date = document.getElementById('datebox')
+
+const myDate = new Date();
+const dayArray = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+day.innerHTML = dayArray[myDate.getDay()]
+date.innerHTML = myDate.getDate();
